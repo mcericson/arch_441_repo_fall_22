55235dcb35bbd985f36c78f2cde3671b2adf957f
mcericson <mark.ericson@woodbury.edu>
2022-11-07 13:01:48 -0800

    Update condit_cube_inclass.py

--- 11_week/condit_cube_inclass.py
+++ 11_week/condit_cube_inclass.py
@@ -1,8 +1,8 @@
-import rhinoscriptsyntax
+import rhinoscriptsyntax as rs
 
 
 
-def cubic_grid(x_num, y_num, z_num, space):
+def cubic_grid(x_num, y_num, z_num,space ):
     points = []
     for i in range(0, x_num, space):
         x = i
@@ -33,3 +33,30 @@ def center_cube(center, radius):
     
     cube = rs.AddBox(points)
     return(cube)
+
+def cull_with_sphere(point_list, center, radius):
+    new_list = []
+    for i in point_list:
+        distance = rs.Distance(center, i)
+        if distance >= radius:
+            new_list.append(i)
+    point_list = []
+    return new_list
+
+def main():
+    rs.EnableRedraw(False)
+    space = 1
+    cube_dim = 20
+    point_list = cubic_grid(cube_dim, cube_dim, cube_dim, space)
+    new_list_1 = cull_with_sphere(point_list, (0,0,0), cube_dim/2)
+    new_list_2 = cull_with_sphere(new_list_1, (cube_dim, 0 ,0), cube_dim/2)
+    new_list_3 = cull_with_sphere(new_list_2, (cube_dim, cube_dim,0), cube_dim/2)
+    new_list_4 = cull_with_sphere(new_list_3, (0, cube_dim,0), cube_dim/2)
+    new_list_5 = cull_with_sphere(new_list_4, (0,0,cube_dim), cube_dim/2)
+    new_list_6 = cull_with_sphere(new_list_5, (cube_dim, 0 ,cube_dim), cube_dim/2)
+    new_list_7 = cull_with_sphere(new_list_6, (cube_dim, cube_dim,cube_dim), cube_dim/2)
+    new_list_8 = cull_with_sphere(new_list_7, (0, cube_dim,cube_dim), cube_dim/2)
+    for i in new_list_8:
+        center_cube(i, float(space/2))
+
+main()
