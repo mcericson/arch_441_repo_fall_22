288ef98b49cf80113efbf02b566191b01ea77ec3
Mark Ericson <ericson.mark.c@gmail.com>
2022-12-08 15:43:30 -0800

    update to viewport tools

--- 13_week/rgb_cube_rotate.py
+++ 13_week/rgb_cube_rotate.py
@@ -5,11 +5,118 @@ import rhinoscriptsyntax as rs
 import scriptcontext as sc
 import Rhino
 import System
-from imp import reload
+
 
 #user libraries 
-import viewport_tools as vt
-reload(vt)
+def capture_view(Scale,FileName,NewFolder):
+    
+    """
+    Captures the active viewport as a .png file
+    Scale:  Float value to multiply existing viewport dimensions by
+    FileName:  Name of file that you would like to save
+    NewFolder:  Name of folder to be created on your desktop
+
+    example:
+    capture_view(2.0, "test_drawing", "drawing_folder") 
+    """
+
+    view = sc.doc.Views.ActiveView
+    if view:
+        view_capture = Rhino.Display.ViewCapture()
+        view_capture.Width = view.ActiveViewport.Size.Width*Scale
+        view_capture.Height = view.ActiveViewport.Size.Height*Scale
+        view_capture.ScaleScreenItems = False
+        view_capture.DrawAxes = False
+        view_capture.DrawGrid = False
+        view_capture.DrawGridAxes = False
+        view_capture.TransparentBackground = False
+        bitmap = view_capture.CaptureToBitmap(view)
+        if bitmap:
+            #locate the desktop and get path
+            folder = System.Environment.SpecialFolder.Desktop
+            path = System.Environment.GetFolderPath(folder)
+            #convert foldername and file name sto string
+            FName = str(NewFolder)
+            File = str(FileName)
+            #combine foldername and desktop path
+            Dir = System.IO.Path.Combine(path,FName)
+            #creat path to the new folder
+            NFolder = System.IO.Directory.CreateDirectory(Dir)
+            Dir = System.IO.Path.Combine(Dir,FileName +".png")
+            print (Dir)
+            #save the file
+            bitmap.Save(Dir, System.Drawing.Imaging.ImageFormat.Png);
+            Rhino.Display
+    return view
+
+def set_viewports_mode(display_mode="Rendered"):
+    """
+    sets all viewports display mode
+    display_mode: Sets the mode of of display.  If None == "Rendered".  
+    "Rendered", "Wireframe", "Ghosted", "Shaded", "X-Ray", "Arctic", "Technical", "Artistic", "Pen"
+    set_viewports_mode
+    """
+    rs.EnableRedraw(True)
+    views = rs.ViewNames()
+    print(views)
+    for view in views:
+        rs.ViewDisplayMode(view,display_mode)
+        rs.ShowGrid(view,show=False)
+        rs.ShowGridAxes(view,show=False)
+        rs.ShowWorldAxes(view,show=False)
+
+def set_axon_view(rotate_right, rotate_up, view_name):
+    """ 
+    sets an axonometric view by rotating the view.
+    rotate_right:  Angle in degrees to rotate the right
+    rotate_up:  Angle in degrees to rotate the model up
+    view_name:  Saved name of view 
+    example:
+    set_axon_view(45, 120, "Top")
+
+    """
+    rs.EnableRedraw(True)
+    rs.ViewProjection(mode=1)
+    rs.RotateView(None, 0, angle=float(rotate_right))
+    rs.RotateView( None, 3, angle=float(rotate_up))
+    
+
+def set_display_mode(view_name, display_mode="Rendered"):
+    """
+    Sets the disaplay mode of a specific viewport
+    view_name: name of the view
+    display_mode: Sets the mode of of display.  If None == "Rendered".  
+    "Rendered", "Wireframe", "Ghosted", "Shaded", "X-Ray", "Arctic", "Technical", "Artistic", "Pen"
+    example:
+    set_display_mode("Top", display_mode="Wireframe")
+        """
+    rs.EnableRedraw(True)
+    viewport = sc.doc.Views.Find(view_name, False).ActiveViewport
+    mode = Rhino.Display.DisplayModeDescription.FindByName("Rendered")
+    viewport.DisplayMode = mode
+
+def create_parallel_view(view_name, size):
+    """Creates a new viewport in top, parallel view.
+    view_name:  name of view to be added
+    size:  Tuple of X and Y dimensions in pixels of the viewport to be created (x_size, y_size)
+    create_parallel_view("Axo", (800,800), display_mode="Rendered")
+    """
+    size_x, size_y = size
+    view = Rhino.Display.DefinedViewportProjection.Top
+    dimensions = System.Drawing.Rectangle(20,20,size_x,size_y)
+    sc.doc.Views.Add(view_name, view, dimensions,True)
+  
+
+def zoom_scale(zoom_factor, view_name):
+    """
+    Zooms the active window by a value
+    example:
+    zoom_scale(.75)
+    """
+    rs.EnableRedraw(True)
+    factor = rs.ViewCameraLens() * float(zoom_factor)
+    rs.ViewCameraLens(view_name, factor)
+
 
 
 def save_obj(Objects,FileName,NewFolder):
@@ -129,18 +236,18 @@ def main():
     rgb_cube(cube_dim, cube_dim, cube_dim, 1)
     rs.EnableRedraw(True)
     view_name = "axo_cube_2"
-    vt.create_parallel_view(view_name, (800, 800))
-    vt.set_axon_view(45, 120, view_name)
+    create_parallel_view(view_name, (800, 800))
+    set_axon_view(45, 120, view_name)
     rs.ZoomExtents()
-    vt.zoom_scale(.75, view_name)
-    vt.set_display_mode(view_name, "Rendered")
+    zoom_scale(.75, view_name)
+    set_display_mode(view_name, "Rendered")
     animate = rs.GetString("Save animation?", "No", ["yes", "no"])
     if animate == "yes":
         for i in range(360):
             rs.Sleep(1)
-            vt.set_axon_view(1,0, view_name)
+            set_axon_view(1,0, view_name)
             animate_name = "rgb_" + str("%04d"%i)
-            vt.capture_view(2.0, animate_name, "rgb_animation")
+            capture_view(2.0, animate_name, "rgb_animation")
         
 
     
